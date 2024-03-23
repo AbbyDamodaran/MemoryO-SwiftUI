c442682444c4cb166fb4f13f74f0e739322f8a75
Abby Damodaran <abbydamodaran@Abbys-MacBook-Air.local>
2024-03-22 22:27:50 -0700

    Initial commit

---
+++ MemoryO/MemoryOApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryOApp.swift
+//  MemoryO
+//
+//  Created by Abby Damodaran on 3/22/24.
+//
+
+import SwiftUI
+
+@main
+struct MemoryOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
